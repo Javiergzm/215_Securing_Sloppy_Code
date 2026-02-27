725fcbf7c083529128bc90706fd80ee2189905f3
Javiergzm <Javier.guzman@stu.vertexacademies.org>
2026-02-27 19:17:12 +0000

    needs more more more work

--- runtime_errors/module.py
+++ runtime_errors/module.py
@@ -3,19 +3,27 @@ import pwd
 
 def isdigit(string):    #"333" or maybe "banana"
     digits = ["0","1","2","3","4","5","6","7","8","9"]
-    letters =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    index = 0
+    count: 0
+
+    while index <= len(string)-1:
+        letter = string[digits]
+
+        if letter not in digits:
 
 
-    for char in string:
-        pass
+
+
+            count = count + 1
+        index += 1
+
     
 
 
     # If the string has any Letters return False
-    if pwd == letters:
-        return False 
+    #if pwd == letters:
+     #   return False 
 
     # If the string has only numbers return True
-    if pwd == digits:
-        return True
-    pass
+    #if pwd == digits:
+     #   return True 
